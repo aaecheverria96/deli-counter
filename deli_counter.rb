0b85d9528827b33f6226d2725c229b1cad7a5e44
Your Username <you@example.com>
2020-10-24 23:51:58 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+
+def line(katz_deli) 
+    if katz_deli.length == 0 
+        puts "The line is currently empty." 
+    else 
+        string = "The line is currently:"
+        katz_deli.each_with_index do |customer, index| 
+        string << " #{index + 1}. #{customer}" 
+    end 
+    puts string 
+end 
+end 
+
+def take_a_number(katz_deli, string) 
+    if katz_deli.length ==0
+    katz_deli << string 
+    puts "Welcome, #{string}. You are number #{katz_deli.length} in line." 
+    print katz_deli  
+    elsif katz_deli.length > 0 
+        katz_deli << string 
+        puts "Welcome, #{string}. You are number #{katz_deli.length} in line."
+    end 
+end 
+
+def now_serving(katz_deli) 
+    if katz_deli.length == 0 
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli[0]}." 
+        katz_deli.shift 
+    end 
+end 
+
